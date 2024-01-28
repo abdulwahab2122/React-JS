f2522328bf609b6c4bdca8935c2037be010508ce
Abdul Wahab <115396116+abdulwahab2122@users.noreply.github.com>
2024-01-28 17:17:57 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+import score from "./images/score.png"
+import feedback from "./images/feedback.png"
+import rank from "./images/rank.png"
+export default[
+    {
+        img : score,
+        name: "Score",
+        des: "Scoring module analyzes each CV’s content and compares it to the job requirements, assigning a compatibility score."
+    },
+    {
+        img : rank,
+        name: "Rank",
+        des: "Rank module ranks the CVs based on their compatibility with the job requirements."
+    },
+    {
+        img : feedback,
+        name: "Feedback",
+        des: "Feedback module provides a detailed report on each CV, highlighting its strengths and weaknesses."
+    }
+    
+]
